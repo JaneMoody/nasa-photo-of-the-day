e8a9e051859b64d30841d40d1b57e24f58d8468f
JaneMoody <janeemoody2@gmail.com>
2020-12-09 20:18:41 -0500

    Got the MVP just gotta do some styling so this doesn't look boring AFgit add .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import DateInput from "./Components/DateInput";
 import "./App.css";
+import Photo from "./Components/Photo.js";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    date: "",
+    photo: "",
+  };
+
+  changeDate = (e) => {
+    e.preventDefault();
+    let dateFromInput = e.target[0].value;
+    this.setState({ date: dateFromInput });
+    this.getPhoto(dateFromInput);
+  };
+
+  componentDidMount() {
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=GNTFSQRo7Wtzmc51JvivCc00L3Ist2MWtfTcLVwG`
+    )
+      .then((response) => response.json())
+      .then((json) => this.setState({ photo: json }));
+  }
+
+  getPhoto = (date) => {
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=GNTFSQRo7Wtzmc51JvivCc00L3Ist2MWtfTcLVwG`
+    )
+      .then((response) => response.json())
+      .then((photoData) => this.setState({ photo: photoData }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>NASA's Picture of the day</h1>
+        <DateInput changeDate={this.changeDate} />
+        <Photo photo={this.state.photo} />
+      </div>
+    );
+  }
 }
 
 export default App;
